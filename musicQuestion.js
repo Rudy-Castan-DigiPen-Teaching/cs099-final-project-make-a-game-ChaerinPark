5e987477efb2413bdf532ce444205f87121be2d3
ChaerinPark <chaerin.park@digipen.edu>
2021-06-20 17:41:05 +0900

    cut sound files and edit conditional statements

--- musicQuestion.js
+++ musicQuestion.js
@@ -32,36 +32,39 @@ class musicQuestion
             fill(255);        
             rect(x + ((white_key_width*0.5)*column+1), y, (white_key_width*0.5), (white_key_height*0.5));
         }
-        for(let i=0; i<musicQ_sound.length; i++)
+        fill(255, 90, 110);
+        if(pianoC.isPlaying() === true || celestaC.isPlaying() === true || accordionC.isPlaying() === true)
         {
-            fill(255, 90, 110);
-            if((musicQ_sound[i] == pianoC || musicQ_sound[i] == celestaC || musicQ_sound[i] == accordionC) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x+1, y, (white_key_width*0.5), (white_key_height*0.5));
-            } else if((musicQ_sound[i] == pianoD || musicQ_sound[i] == celestaD || musicQ_sound[i] == accordionD) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + ((white_key_width*0.5)+1), y, (white_key_width*0.5), (white_key_height*0.5));
-            } else if((musicQ_sound[i] == pianoE || musicQ_sound[i] == celestaE || musicQ_sound[i] == accordionE) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + ((white_key_width*0.5)*2+1), y, (white_key_width*0.5), (white_key_height*0.5));
-            } else if((musicQ_sound[i] == pianoF || musicQ_sound[i] == celestaF || musicQ_sound[i] == accordionF) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + ((white_key_width*0.5)*3+1), y, (white_key_width*0.5), (white_key_height*0.5));
-            } else if((musicQ_sound[i] == pianoG || musicQ_sound[i] == celestaG || musicQ_sound[i] == accordionG) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + ((white_key_width*0.5)*4+1), y, (white_key_width*0.5), (white_key_height*0.5));
-            } else if((musicQ_sound[i] == pianoA || musicQ_sound[i] == celestaA || musicQ_sound[i] == accordionA) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + ((white_key_width*0.5)*5+1), y, (white_key_width*0.5), (white_key_height*0.5));
-            } else if((musicQ_sound[i] == pianoB || musicQ_sound[i] == celestaB || musicQ_sound[i] == accordionB) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + ((white_key_width*0.5)*6+1), y, (white_key_width*0.5), (white_key_height*0.5));
-            }
-            for(let column=0; column < 7; column++)
-            {
-                fill(255);        
-                text(What_Key_W[column], x + ((white_key_width*0.5)*column+35), y + white_key_height*5/12);    
-            }
+            rect(x+1, y, (white_key_width*0.5), (white_key_height*0.5));
+        }
+        if(pianoD.isPlaying() === true || celestaD.isPlaying() === true || accordionD.isPlaying() === true)
+        {
+            rect(x + ((white_key_width*0.5)+1), y, (white_key_width*0.5), (white_key_height*0.5));
+        }
+        if(pianoE.isPlaying() === true || celestaE.isPlaying() === true || accordionE.isPlaying() === true)
+        {
+            rect(x + ((white_key_width*0.5)*2+1), y, (white_key_width*0.5), (white_key_height*0.5));
+        }
+        if(pianoF.isPlaying() === true || celestaF.isPlaying() === true || accordionF.isPlaying() === true)
+        {
+            rect(x + ((white_key_width*0.5)*3+1), y, (white_key_width*0.5), (white_key_height*0.5));
+        }
+        if(pianoG.isPlaying() === true || celestaG.isPlaying() === true || accordionG.isPlaying() === true)
+        {
+            rect(x + ((white_key_width*0.5)*4+1), y, (white_key_width*0.5), (white_key_height*0.5));
+        }
+        if(pianoA.isPlaying() === true || celestaA.isPlaying() === true || accordionA.isPlaying() === true)
+        {
+            rect(x + ((white_key_width*0.5)*5+1), y, (white_key_width*0.5), (white_key_height*0.5));
+        }
+        if(pianoB.isPlaying() === true || celestaB.isPlaying() === true || accordionB.isPlaying() === true)
+        {
+            rect(x + ((white_key_width*0.5)*6+1), y, (white_key_width*0.5), (white_key_height*0.5));
+        }
+        for(let column=0; column < 7; column++)
+        {
+            fill(255);        
+            text(What_Key_W[column], x + ((white_key_width*0.5)*column+35), y + white_key_height*5/12);    
         }
         pop();
     }
@@ -81,34 +84,34 @@ class musicQuestion
                 rect(x + (((black_key_width + black_key_interval)*0.5)*column+1), y, (black_key_width*0.5), (black_key_height*0.5));
             }
         }
-
-        for(let i=0; i<musicQ_sound.length; i++)
+        fill(255, 90, 110);
+        if(pianoCs.isPlaying() === true || celestaCs.isPlaying() === true || accordionCs.isPlaying === true)
         {
-            fill(255, 90, 110);
-            if((musicQ_sound[i] == pianoCs || musicQ_sound[i] == celestaCs || musicQ_sound[i] == accordionCs) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x+1, y, (black_key_width*0.5), (black_key_height*0.5));    
-            } else if((musicQ_sound[i] == pianoDs || musicQ_sound[i] == celestaDs || musicQ_sound[i] == accordionDs) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + (((black_key_width + black_key_interval)*0.5)+1), y, (black_key_width*0.5), (black_key_height*0.5));    
-            } else if((musicQ_sound[i] == pianoFs || musicQ_sound[i] == celestaFs || musicQ_sound[i] == accordionFs) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + (((black_key_width + black_key_interval)*0.5)*3+1), y, (black_key_width*0.5), (black_key_height*0.5));    
-            } else if((musicQ_sound[i] == pianoGs || musicQ_sound[i] == celestaGs || musicQ_sound[i] == accordionGs) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + (((black_key_width + black_key_interval)*0.5)*4+1), y, (black_key_width*0.5), (black_key_height*0.5));    
-            } else if((musicQ_sound[i] == pianoAs || musicQ_sound[i] == celestaAs || musicQ_sound[i] == accordionAs) && musicQ_sound[musicQ_sound.length-2].isPlaying() === true)
-            {
-                rect(x + (((black_key_width + black_key_interval)*0.5)*5+1), y, (black_key_width*0.5), (black_key_height*0.5));    
-            }
+        rect(x+1, y, (black_key_width*0.5), (black_key_height*0.5));    
+        }
+        if(pianoDs.isPlaying() === true || celestaDs.isPlaying() === true || accordionDs.isPlaying === true)
+        {
+        rect(x + (((black_key_width + black_key_interval)*0.5)+1), y, (black_key_width*0.5), (black_key_height*0.5));    
+        }
+        if(pianoFs.isPlaying() === true || celestaFs.isPlaying() === true || accordionFs.isPlaying === true)
+        {
+        rect(x + (((black_key_width + black_key_interval)*0.5)*3+1), y, (black_key_width*0.5), (black_key_height*0.5));    
+        }
+        if(pianoGs.isPlaying() === true || celestaGs.isPlaying() === true || accordionGs.isPlaying === true)
+        {
+        rect(x + (((black_key_width + black_key_interval)*0.5)*4+1), y, (black_key_width*0.5), (black_key_height*0.5));    
+        }
+        if(pianoAs.isPlaying() === true || celestaAs.isPlaying() === true || accordionAs.isPlaying === true)
+        {
+        rect(x + (((black_key_width + black_key_interval)*0.5)*5+1), y, (black_key_width*0.5), (black_key_height*0.5));    
+        }
 
-            for(let column = 0; column < 6; column++)
+        for(let column = 0; column < 6; column++)
+        {
+            if(column !== 2)
             {
-                if(column !== 2)
-                {
-                    fill(255);
-                    text(What_Key_B[column], x + (((black_key_width + black_key_interval)*0.5)*column+25), y + black_key_height*5/12);    
-                }
+                fill(255);
+                text(What_Key_B[column], x + (((black_key_width + black_key_interval)*0.5)*column+25), y + black_key_height*5/12);    
             }
         }
         pop();
